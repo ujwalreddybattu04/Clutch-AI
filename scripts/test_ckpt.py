1ffece3d2e664c6f402ac1b08d6f22fe1ae458aa
Ujwal Reddy Battu <168129796+ujwalreddybattu04@users.noreply.github.com>
2026-01-29 21:36:31 +0530

    Update test_ckpt.py

--- scripts/test_ckpt.py
+++ scripts/test_ckpt.py
@@ -28,7 +28,7 @@ print("Loaded! iter_num =", ckpt.get("iter_num", "unknown"))
 
 enc = tiktoken.get_encoding("gpt2")
 
-instruction = "If a train travels 60 km in 1 hour, how long will it take to travel 90 km?"
+instruction = "A is older than B, B is older than C. Who is the youngest?"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
 
 idx = torch.tensor([enc.encode(prompt)], dtype=torch.long, device=device)
