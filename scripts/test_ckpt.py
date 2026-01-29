57636b88703350e8f73abeb175fbfea90a273e3b
Ujwal Reddy Battu <168129796+ujwalreddybattu04@users.noreply.github.com>
2026-01-29 21:34:27 +0530

    Update test_ckpt.py

--- scripts/test_ckpt.py
+++ scripts/test_ckpt.py
@@ -28,7 +28,7 @@ print("Loaded! iter_num =", ckpt.get("iter_num", "unknown"))
 
 enc = tiktoken.get_encoding("gpt2")
 
-instruction = "Explain what is kinatic energy ??"
+instruction = "what are different types of forces ??"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
 
 idx = torch.tensor([enc.encode(prompt)], dtype=torch.long, device=device)
