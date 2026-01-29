ddd6dfcdc1c1bc9c46d6357af8c165494ba24f7d
Ujwal Reddy Battu <168129796+ujwalreddybattu04@users.noreply.github.com>
2026-01-29 21:36:56 +0530

    Update test_ckpt.py

--- scripts/test_ckpt.py
+++ scripts/test_ckpt.py
@@ -28,7 +28,7 @@ print("Loaded! iter_num =", ckpt.get("iter_num", "unknown"))
 
 enc = tiktoken.get_encoding("gpt2")
 
-instruction = "A is older than B, B is older than C. Who is the youngest?"
+instruction = "If today is Monday, what day will it be after 10 days?"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
 
 idx = torch.tensor([enc.encode(prompt)], dtype=torch.long, device=device)
