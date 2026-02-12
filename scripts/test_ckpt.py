81d025f191e105cc3cf3bb9135109e5f2ed4c8fe
Ujwal Reddy Battu <ujwalreddybattu@gmail.com>
2026-02-12 20:51:39 +0530

    updated prompts

--- scripts/test_ckpt.py
+++ scripts/test_ckpt.py
@@ -28,7 +28,7 @@ print("Loaded! iter_num =", ckpt.get("iter_num", "unknown"))
 
 enc = tiktoken.get_encoding("gpt2")
 
-instruction = "What is early stopping?"
+instruction = "What is early stopping in deep learning?"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
 
 idx = torch.tensor([enc.encode(prompt)], dtype=torch.long, device=device)
