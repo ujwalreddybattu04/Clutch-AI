3af5f7b205714a12571bb76a2161323751cb444b
Ujwal Reddy Battu <ujwalreddybattu@gmail.com>
2026-02-16 21:30:24 +0530

    updated prompts

--- scripts/test_ckpt.py
+++ scripts/test_ckpt.py
@@ -28,7 +28,7 @@ print("Loaded! iter_num =", ckpt.get("iter_num", "unknown"))
 
 enc = tiktoken.get_encoding("gpt2")
 
-instruction = "What is bidireional lstm ?"
+instruction = "What is transformers ?"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
 
 idx = torch.tensor([enc.encode(prompt)], dtype=torch.long, device=device)
