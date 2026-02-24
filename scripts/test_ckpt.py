3812adffbab283c65e1ab94aa421bf912281f637
Ujwal Reddy Battu <ujwalreddybattu@gmail.com>
2026-02-24 19:09:23 +0530

    updated prompts

--- scripts/test_ckpt.py
+++ scripts/test_ckpt.py
@@ -28,7 +28,7 @@ print("Loaded! iter_num =", ckpt.get("iter_num", "unknown"))
 
 enc = tiktoken.get_encoding("gpt2")
 
-instruction = "how to reduce llm hallucinations?"
+instruction = "how to GSAP In web development?"
 prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
 
 idx = torch.tensor([enc.encode(prompt)], dtype=torch.long, device=device)
